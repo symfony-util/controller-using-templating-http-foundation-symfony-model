889464be1d26380282a9dc829a4a6340d4a28ad4
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-28 14:19:22 -0400

    Update IndexController.php

--- IndexController.php
+++ IndexController.php
@@ -35,6 +35,6 @@ class IndexController
     public function __invoke(Request $request = null)
     {
         // return new Response($this->templating->render($this->template, $this->viewModel->index($this->actionModel->index($request))));
-        return new Response($this->templating->render($this->template, ($this->model)($request)));
+        return new Response($this->templating->render($this->template, $this->model->__invoke($request)));
     }
 }
