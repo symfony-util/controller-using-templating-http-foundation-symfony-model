674e2db2c0e0fc130277ccc9e682cd7828d0b013
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-28 16:02:54 -0400

    Create ShowControllerTest.php

---
+++ tests/ShowControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Symfony-Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Bridge\Twig\TwigEngine;
+use Symfony\Component\Templating\TemplateNameParser;
+use SymfonyUtil\Component\HttpFoundation\NullStringControllerModel;
+use SymfonyUtil\Component\TemplatingHttpFoundation\ShowController;
+
+/**
+ * @covers \SymfonyUtil\Component\TemplatingHttpFoundation\IndexController
+ */
+final class IndexControllerTest extends TestCase
+{
+    public function testCanBeCreated()
+    {
+        $this->assertInstanceOf(
+            // ::class, // 5.4 < php
+            'SymfonyUtil\Component\TemplatingHttpFoundation\ShowController',
+            new IndexController(new NullControllerModel(), new TwigEngine(
+                new Twig_Environment(new Twig_Loader_Array(['index.html.twig' => 'Hello World!'])),
+                new TemplateNameParser()
+            ))
+        );
+    }
+
+    public function testReturnsResponse()
+    {
+        $controller = new ShowController(new NullStringControllerModel(), new TwigEngine(
+            new Twig_Environment(new Twig_Loader_Array(['index.html.twig' => 'Hello World!'])),
+            new TemplateNameParser()
+        ));
+        $this->assertInstanceOf(
+            // Response::class, // 5.4 < php
+            'Symfony\Component\HttpFoundation\Response',
+            $controller('')
+        );
+    }
+}
